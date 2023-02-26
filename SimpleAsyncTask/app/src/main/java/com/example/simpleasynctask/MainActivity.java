28d1952a239489b0ff0316ceaaecdec83d7646f6
raymondj-pace <rj07609p@pace.edu>
2023-02-26 16:30:51 -0500

    Initial commit

---
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.simpleasynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Save the state of the TextView
+    private static final String TEXT_STATE = "currentText";
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textView1);
+    }
+
+    public void startTask(View view) {
+        // Put a message in the text view
+        mTextView.setText(R.string.start_text);
+
+        // Start the AsyncTask
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+    // Save the contents of TextView to restore when needed (device rotation, etc)
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the state of the TextView
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+}
---
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
@@ -0,0 +1,51 @@
+package com.example.simpleasynctask;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
+    private WeakReference<TextView> mTextView;
+
+    SimpleAsyncTask(TextView tv) {
+        mTextView = new WeakReference<>(tv);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        int s = n * 1000;
+
+        try {
+
+            for (int i = 0; i < 10; i++) {
+
+                publishProgress(i*10);
+                Thread.sleep(s/10);
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+
+        String s = String.valueOf(values[0]) + "%";
+
+        mTextView.get().setText(s);
+    }
+}
