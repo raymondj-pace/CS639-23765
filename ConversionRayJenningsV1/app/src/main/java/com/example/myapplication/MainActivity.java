a5db6c1908611adaf3db8857f6684832e72ce96e
raymondj-pace <rj07609p@pace.edu>
2023-03-25 11:06:10 -0400

    Small commits to avoid git error

---
+++ ConversionRayJenningsV1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEuroAmount;
+
+    private TextView mDollarAmount;
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Hide keyboard if it's showing after creation
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm.isActive()) {
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEuroAmount = findViewById(R.id.editTextNumberDecimal);
+
+        mDollarAmount = findViewById(R.id.textView4);
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+
+    public void showDollarAmount(View view) {
+        // Get the search string from the input field.
+
+        // Hide keyboard if it's showing after creation
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (imm.isActive()) {
+            imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+        }
+
+
+        String euros_string = mEuroAmount.getText().toString();
+        if (euros_string.isEmpty()) {
+
+            if (imm.isActive()) {
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            }
+
+            Toast toast = Toast.makeText(this, "Enter Euro decimal amount", Toast.LENGTH_SHORT);
+            toast.show();
+
+            return;
+        }
+
+        double euros = Double.parseDouble(mEuroAmount.getText().toString());
+
+        double dollars = (euros / 0.88);
+
+        dollars = dollars * 100;
+        dollars = Math.round(dollars);
+        dollars = dollars / 100;
+
+        String dollar_str = String.valueOf(dollars);
+
+        // mEuroAmount.setText("");
+
+        mDollarAmount.setText("Conversion = $" + dollar_str);
+
+        // mEuroAmount.requestFocus();
+
+        imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm.isActive()) {
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+}
